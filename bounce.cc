d7f3f06deb513b994f883d48178b308b4ae8b2fc
Ryan Pepper <ryan.pepper@soton.ac.uk>
2016-10-03 12:03:59 +0100

    Add move

--- bounce.cc
+++ bounce.cc
@@ -2,28 +2,33 @@
 
 #include <iostream>
 
+void move(double *particlePosition, double *particleSpeed, int minColumn, int maxColumn) {
+	*particlePosition += *particleSpeed;
+	if (*particlePosition >= maxColumn) {
+		*particlePosition = 2*maxColumn - *particlePosition;
+		*particleSpeed = -*particleSpeed;
+	}
+	else if (*particlePosition <= minColumn) {
+		*particlePosition = -*particlePosition;
+		*particleSpeed = -*particleSpeed;
+	}
+}
+
 int main() {
 
-  char particleSymbol = 'x';
+  std::string particleSymbol = ":)";
   double particlePosition = 0;
   double particleSpeed = 6.3;
   int maxColumn = 80;
   int timeStep = 0;
   int stopTime = 60;
-
+  double minColumn = 0;
   while (timeStep < stopTime) {
     for (int i = 0; i < particlePosition; i++) {
       std::cout << " ";
     }
+    move(&particlePosition, &particleSpeed, minColumn, maxColumn);
     std::cout << particleSymbol << std::endl;
-    particlePosition += particleSpeed;
-    if (particlePosition >= maxColumn) {
-      particlePosition = 2*maxColumn - particlePosition;
-      particleSpeed = -particleSpeed;
-    } else if (particlePosition <= 0) {
-      particlePosition = -particlePosition;
-      particleSpeed = -particleSpeed;
-    }    
     timeStep++;
   }
 }
